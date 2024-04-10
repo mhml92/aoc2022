1d716cbd769b9dfa8214a298f4574f614ab2e33e
MLK <mikkel.levisen@lego.com>
2024-04-10 10:29:14 +0200

    Tests pass: auto-commit

--- tests/day_01/test_calorie_counting.py
+++ tests/day_01/test_calorie_counting.py
@@ -1,23 +1,26 @@
+import pytest
+
 from aoc2022.day_01.calorie_counting import split_by_elf, most_calories_carried_by_elf
 
 
-def read_test_input_data() -> str:
+@pytest.fixture
+def input_data() -> str:
     with open('tests/day_01/data.txt') as f:
         return f.read()
 
 
-def test_read_input_data():
-    assert isinstance(read_test_input_data(), str)
+def test_read_input_data(input_data: str):
+    assert isinstance(input_data, str)
 
 
-def test_most_calories_carried_by_elf():
+def test_most_calories_carried_by_elf(input_data: str):
     assert most_calories_carried_by_elf("1") == 1
     assert most_calories_carried_by_elf("1\n\n2") == 2
     assert most_calories_carried_by_elf("1\n2\n\n2") == 3
-    assert most_calories_carried_by_elf(read_test_input_data()) == 24000
+    assert most_calories_carried_by_elf(input_data) == 24000
 
 
-def test_split_by_elf():
+def test_split_by_elf(input_data: str):
     assert split_by_elf("") == []
     assert split_by_elf("1") == ["1"]
     assert split_by_elf("1\n1") == ["1\n1"]
@@ -25,4 +28,4 @@ def test_split_by_elf():
     assert split_by_elf("1\n1\n\n2") == ["1\n1", "2"]
     assert split_by_elf("1\n1\n\n2\n\n") == ["1\n1", "2"]
 
-    assert len(split_by_elf(read_test_input_data())) == 5
+    assert len(split_by_elf(input_data)) == 5

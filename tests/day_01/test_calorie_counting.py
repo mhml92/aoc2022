e764edba30ee2d29edd7600f536020e864176304
MLK <mikkel.levisen@lego.com>
2024-04-10 09:41:38 +0200

    Tests pass: auto-commit

--- tests/day_01/test_calorie_counting.py
+++ tests/day_01/test_calorie_counting.py
@@ -1,15 +1,16 @@
-def read_test_data() -> list[str]:
+def read_test_data() -> str:
     with open('tests/day_01/data.txt') as f:
-        return f.read().splitlines()
+        return f.read()
 
 
-def test_read_data_as_list():
-    assert isinstance(read_test_data(), list)
+def test_read_data():
+    assert isinstance(read_test_data(), str)
 
 
-def group_by_elf() -> list[list[str]]:
-    return [[], [], [], [], []]
-
-
-def test_group_items_by_elf():
-    assert len(group_by_elf()) == 5
+#def group_by_elf() -> list[list[str]]:
+#    for
+#    return [[], [], [], [], []]
+#
+#
+#def test_group_items_by_elf():
+#    assert len(group_by_elf()) == 5

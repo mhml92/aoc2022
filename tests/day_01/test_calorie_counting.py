de78e49445fd80d42144f148bee3d325ca61235d
MLK <mikkel.levisen@lego.com>
2024-04-10 10:05:35 +0200

    Tests pass: auto-commit

--- tests/day_01/test_calorie_counting.py
+++ tests/day_01/test_calorie_counting.py
@@ -7,14 +7,14 @@ def test_read_input_data():
     assert isinstance(read_input_data(), str)
 
 
-def group_by_elf(input: str) -> list[list[str]]:
+def group_by_elf(input: str) -> list[str]:
     if not input:
         return []
-    return [[input]]
+    return input.split(sep="\n\n")
 
 
 def test_group_items_by_elf():
     assert group_by_elf("") == []
-    assert group_by_elf("1") == [["1"]]
-    assert group_by_elf("1\n1") == [["1\n1"]]
-    assert group_by_elf("1\n\n1") == [["1"], ["1"]]
+    assert group_by_elf("1") == ["1"]
+    assert group_by_elf("1\n1") == ["1\n1"]
+    assert group_by_elf("1\n\n1") == ["1", "1"]

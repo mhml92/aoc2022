98f96cdc967cdabd940a9fbe440d75936db63179
MLK <mikkel.levisen@lego.com>
2024-04-10 10:12:43 +0200

    Tests pass: auto-commit

--- tests/day_01/test_calorie_counting.py
+++ tests/day_01/test_calorie_counting.py
@@ -7,20 +7,21 @@ def test_read_input_data():
     assert isinstance(read_input_data(), str)
 
 
-def group_by_elf(input: str) -> list[str]:
+def split_by_elf(input: str) -> list[str]:
     input = input.strip()
     if not input:
         return []
     return input.strip().split(sep="\n\n")
 
 
-def test_group_items_by_elf():
-    assert group_by_elf("") == []
-    assert group_by_elf("1") == ["1"]
-    assert group_by_elf("1\n1") == ["1\n1"]
-    assert group_by_elf("1\n\n1") == ["1", "1"]
-    assert group_by_elf("1\n1\n\n2") == ["1\n1", "2"]
-    assert group_by_elf("1\n1\n\n2\n\n") == ["1\n1", "2"]
 
-    assert len(group_by_elf(read_input_data())) == 5
+def test_split_by_elf():
+    assert split_by_elf("") == []
+    assert split_by_elf("1") == ["1"]
+    assert split_by_elf("1\n1") == ["1\n1"]
+    assert split_by_elf("1\n\n1") == ["1", "1"]
+    assert split_by_elf("1\n1\n\n2") == ["1\n1", "2"]
+    assert split_by_elf("1\n1\n\n2\n\n") == ["1\n1", "2"]
+
+    assert len(split_by_elf(read_input_data())) == 5
 

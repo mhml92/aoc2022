5cf0c7eaaf3dd8eaee4970dcf33bf1b67ceee9b9
MLK <mikkel.levisen@lego.com>
2024-04-10 09:37:21 +0200

    Tests pass: auto-commit

--- tests/day_01/test_calorie_counting.py
+++ tests/day_01/test_calorie_counting.py
@@ -7,4 +7,9 @@ def test_read_data_as_list():
     assert isinstance(read_test_data(), list)
 
 
+def group_by_elf() -> list[list[str]]:
+    return [[], [], [], [], []]
 
+
+def test_group_items_by_elf():
+    assert len(group_by_elf()) == 5
